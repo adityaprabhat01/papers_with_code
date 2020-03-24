80912fba929caea4cd29ac5d41ae6258fe9c267f
Aditya Prabhat <47939813+adityaprabhat01@users.noreply.github.com>
2020-03-24 11:00:14 +0530

    bug fix and changed to CIFAR10 dataset

--- AlexNet/AlexNet.py
+++ AlexNet/AlexNet.py
@@ -4,41 +4,52 @@ import torch.nn as nn
 from tqdm import tqdm
 import torch.optim as optim
 import torch.nn.functional as F
-from sklearn.model_selection import train_test_split
+from torchvision import transforms, datasets
 
-training_data = np.load('training_data.npy', allow_pickle = True)
+train_data = datasets.CIFAR10('CIFAR10', train = True, 
+                         transform = transforms.Compose([transforms.ToTensor()]),
+                         download = True)
+
+test_data = datasets.CIFAR10('CIFAR10', train = False,
+                             transform = transforms.Compose([transforms.ToTensor()]),
+                             download = True)
+
+train_batch = torch.utils.data.DataLoader(train_data, batch_size = 100, shuffle = True)
+test_batch = torch.utils.data.DataLoader(test_data, batch_size = 1, shuffle = True)
 
 class AlexNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 96, kernel_size = 11, stride = 4, padding = 0)
-        self.conv2 = nn.Conv2d(96, 256, kernel_size = 5, stride = 1, padding = 2)
-        self.conv3 = nn.Conv2d(256, 384, kernel_size = 3, stride = 1, padding = 1)
-        self.conv4 = nn.Conv2d(384, 384, kernel_size = 3, stride = 1, padding = 1)
-        self.conv5 = nn.Conv2d(384, 256, kernel_size = 3, stride = 1, padding = 1)
-        self.fc1 = nn.Linear(9216, 4096)
-        self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, 2)
+        self.conv1 = nn.Conv2d(3, 96, kernel_size = (3, 3), stride = 1, padding = 1) #  32x32
+        '''In the original paper the authors have drastically reduced the resolution of the image but since the image size here 
+        is already very small therefore preserved the dimension in the first convolution operation'''
+        self.conv2 = nn.Conv2d(96, 256, kernel_size = (3, 3), stride = 1, padding = 1) # 16x16
+        self.conv3 = nn.Conv2d(256, 384, kernel_size = (3, 3), stride = 1, padding = 1) #8x8
+        self.conv4 = nn.Conv2d(384, 384, kernel_size = (3, 3), stride = 1, padding = 1) #8x8
+        self.conv5 = nn.Conv2d(384, 256, kernel_size = (3, 3), stride = 1, padding = 1) #8x8
+        self.fc1 = nn.Linear(4096, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.fc3 = nn.Linear(2048, 10)
         
     def convs(self, x):
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size = (3, 3), stride = 2, padding = 0)
+        x = F.max_pool2d(x, kernel_size = (2, 2), stride = 2, padding = 0) # 16x16
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size = (3, 3), stride = 2, padding = 0)
+        x = F.max_pool2d(x, kernel_size = (2, 2), stride = 2, padding = 0) # 8x8
         x = self.conv3(x)
         x = F.relu(x)
         x = self.conv4(x)
         x = F.relu(x)
         x = self.conv5(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size = (3, 3), stride = 2, padding = 0)
+        x = F.max_pool2d(x, kernel_size = (2, 2), stride = 2, padding = 0) #4x4
         return x
         
     def forward(self, x):
         x = self.convs(x)
-        x = x.view(-1, 9216)
+        x = x.view(-1, 4096)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
@@ -50,15 +61,7 @@ class AlexNet(nn.Module):
 alexnet = AlexNet()
 
 optimizer = optim.Adam(alexnet.parameters(), lr = 0.001)
-loss_function = nn.MSELoss()
-
-X = torch.Tensor([i[0] for i in training_data]).view(-1, 227, 227)
-X = X/255.0
-y = torch.Tensor([i[1] for i in training_data])
-del training_data
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-del X
+loss_function = nn.BCELoss()
 
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
@@ -66,35 +69,42 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
     print("Running on cpu")
-
+    
 alexnet.to(device)
 
-BATCH_SIZE = 100
+def one_hot_encoder(labels, num_labels):
+    batch_size = len(labels)
+    one_hot_labels = np.zeros([batch_size, num_labels])
+    for i in range(batch_size):
+        one_hot_labels[i][labels[i]] = 1
+    return one_hot_labels
+
 EPOCHS = 1
 
-for epoch in range(EPOCHS):
-    for i in tqdm(range(0, len(X_train), BATCH_SIZE)):
-        X_batch = X_train[i:i+BATCH_SIZE].view(-1,1,227,227).to(device)
-        y_batch = y_train[i:i+BATCH_SIZE].to(device)
-        alexnet.zero_grad()
-        outputs = alexnet(X_batch)
-        loss = loss_function(outputs, y_batch)
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch: {epoch}. Loss: {loss}")
+def train(train_batch):
+    for epoch in range(EPOCHS):
+        for images, labels in tqdm(train_batch):
+            one_hot_labels = torch.Tensor(one_hot_encoder(labels, 10))
+            alexnet.zero_grad()
+            outputs = alexnet.forward(images.to(device))
+            loss = loss_function(outputs, one_hot.to(device))
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch: {epoch}. Loss: {loss}")
+        
 
-def test(alexnet):
+def test(test_batch):
     correct = 0
     total = 0
-    with torch.no_grad():
-        for i in tqdm(range(len(X_test))):
-            real_class = torch.argmax(y_test[i]).to(device)
-            net_out = alexnet(X_test[i].view(-1, 1, 227, 227).to(device))[0]
-            predicted_class = torch.argmax(net_out)
-
-            if predicted_class == real_class:
-                correct += 1
-            total += 1
+    for images, labels in tqdm(test_batch):
+        labels = labels.to(device)
+        net_out = alexnet(images.to(device))
+        predicted_class = torch.argmax(net_out).to(device)
+        if (predicted_class == labels):
+            correct += 1
+        total += 1
     print("Accuracy: ", round(correct/total, 3))
-
-test(alexnet)
+    
+    
+train(train_batch)
+test(test_batch)
